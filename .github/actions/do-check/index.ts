0828755214272f129df69283ed2cf025e7e29fc1
pngwn <hello@pngwn.io>
2024-01-23 15:39:59 +0000

    asd

--- .github/actions/do-check/index.ts
+++ .github/actions/do-check/index.ts
@@ -8,19 +8,19 @@ async function run() {
 	const token = getInput("github-token");
 	const octokit = getOctokit(token);
 
-	// let output = "";
-	// await exec("git", ["rev-parse", "HEAD"], {
-	// 	listeners: {
-	// 		stdout: (data) => {
-	// 			output = data.toString();
-	// 		},
-	// 		stderr: (data) => {
-	// 			console.log(`stderr: ${data.toString()}`);
-	// 		},
-	// 	},
-	// });
+	let output = "";
+	await exec("git", ["rev-parse", "HEAD"], {
+		listeners: {
+			stdout: (data) => {
+				output = data.toString();
+			},
+			stderr: (data) => {
+				console.log(`stderr: ${data.toString()}`);
+			},
+		},
+	});
 
-	// const sha = output.trim();
+	const sha = output.trim();
 
 	console.log(context.payload.workflow_run);
 
@@ -32,45 +32,45 @@ async function run() {
 
 	console.log(JSON.stringify(wf.data, null, 2));
 
-	// octokit.rest.repos.createCommitStatus({
-	// 	owner: context.repo.owner,
-	// 	repo: context.repo.repo,
-	// 	sha,
-	// 	state: "success",
-	// 	description: "This is a passing test",
-	// 	context: "check/passing",
-	// 	target_url: "https://google.com",
-	// });
+	octokit.rest.repos.createCommitStatus({
+		owner: context.repo.owner,
+		repo: context.repo.repo,
+		sha,
+		state: "success",
+		description: "This is a passing test",
+		context: "check/passing",
+		target_url: "https://google.com",
+	});
 
-	// octokit.rest.repos.createCommitStatus({
-	// 	owner: context.repo.owner,
-	// 	repo: context.repo.repo,
-	// 	sha,
-	// 	state: "failure",
-	// 	description: "This is a failing test",
-	// 	context: "check/failing",
-	// 	target_url: "https://google.com",
-	// });
+	octokit.rest.repos.createCommitStatus({
+		owner: context.repo.owner,
+		repo: context.repo.repo,
+		sha,
+		state: "failure",
+		description: "This is a failing test",
+		context: "check/failing",
+		target_url: "https://google.com",
+	});
 
-	// octokit.rest.repos.createCommitStatus({
-	// 	owner: context.repo.owner,
-	// 	repo: context.repo.repo,
-	// 	sha,
-	// 	state: "error",
-	// 	description: "This is an error test",
-	// 	context: "check/error",
-	// 	target_url: "https://google.com",
-	// });
+	octokit.rest.repos.createCommitStatus({
+		owner: context.repo.owner,
+		repo: context.repo.repo,
+		sha,
+		state: "error",
+		description: "This is an error test",
+		context: "check/error",
+		target_url: "https://google.com",
+	});
 
-	// octokit.rest.repos.createCommitStatus({
-	// 	owner: context.repo.owner,
-	// 	repo: context.repo.repo,
-	// 	sha,
-	// 	state: "pending",
-	// 	description: "This is a pending test",
-	// 	context: "check/pending",
-	// 	target_url: "https://google.com",
-	// });
+	octokit.rest.repos.createCommitStatus({
+		owner: context.repo.owner,
+		repo: context.repo.repo,
+		sha,
+		state: "pending",
+		description: "This is a pending test",
+		context: "check/pending",
+		target_url: "https://google.com",
+	});
 }
 
 run();
