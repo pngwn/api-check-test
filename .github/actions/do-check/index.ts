eb85920b0fc89f2ef4acaac4a188e8b23cb5438e
pngwn <hello@pngwn.io>
2024-01-24 17:21:50 +0000

    asd

--- .github/actions/do-check/index.ts
+++ .github/actions/do-check/index.ts
@@ -5,24 +5,17 @@ async function run() {
 	const token = getInput("token");
 	const pr = getInput("pr");
 	const sha = getInput("sha");
+
+	console.log({ token, pr, sha });
 	const octokit = getOctokit(token);
 
 	if (context.eventName === "workflow_run") {
 	}
 
-	console.log(context.payload.workflow_run);
+	console.log(context);
 
 	const conclusion = context.payload.workflow_run.conclusion;
 
-	const wf = await octokit.rest.actions.getWorkflowRun({
-		owner: context.repo.owner,
-		repo: context.repo.repo,
-		run_id: context.payload.workflow_run.id,
-	});
-
-	console.log(JSON.stringify(wf.data, null, 2));
-
-	// const _workflow_name = wf.data.name || "Unknown Workflow";
 	const _workflow_name =
 		context.payload.workflow_run.name || "Unknown Workflow";
 	const _status = context.payload.workflow_run.status;
@@ -60,36 +53,6 @@ async function run() {
 		context: _workflow_name,
 		target_url: "https://google.com",
 	});
-
-	// octokit.rest.repos.createCommitStatus({
-	// 	owner: context.repo.owner,
-	// 	repo: context.repo.repo,
-	// 	sha,
-	// 	state: "failure",
-	// 	description: "This is a failing test",
-	// 	context: "check/failing",
-	// 	target_url: "https://google.com",
-	// });
-
-	// octokit.rest.repos.createCommitStatus({
-	// 	owner: context.repo.owner,
-	// 	repo: context.repo.repo,
-	// 	sha,
-	// 	state: "error",
-	// 	description: "This is an error test",
-	// 	context: "check/error",
-	// 	target_url: "https://google.com",
-	// });
-
-	// octokit.rest.repos.createCommitStatus({
-	// 	owner: context.repo.owner,
-	// 	repo: context.repo.repo,
-	// 	sha,
-	// 	state: "pending",
-	// 	description: "This is a pending test",
-	// 	context: "check/pending",
-	// 	target_url: "https://google.com",
-	// });
 }
 
 run();
